33ab06b96ccb8ef8090504c3b777e0dd31f8c731
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-07-10 09:29:43 -0600

    ex1 passing tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,12 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_by_index = {}
+
+    for i in range(len(weights)):
+        if limit - weights[i] in weight_by_index:
+            return [i, weight_by_index[limit - weights[i]]]
+        else: 
+            weight_by_index[weights[i]] = i
 
     return None
