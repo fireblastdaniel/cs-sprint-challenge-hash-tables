c6128dd49de892cb401f86ffe622b5d123b847c3
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-07-10 10:21:42 -0600

    ex5 passes tests

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,19 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
+    f_dict = {}
+    result = []
     # Your code here
+    for f in files:
+        temp = f.split('/')
+        if temp[-1] in f_dict:
+            f_dict[temp[-1]].append(f)
+        else:
+            f_dict[temp[-1]] = [f]
+
+    for q in queries:
+        if q in f_dict:
+            result += f_dict[q]
 
     return result
 
